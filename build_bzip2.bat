7e7869a50224eab787dcee5df4aed8c8a19722ec
Tobias Klein <contact@tklein.info>
2020-02-09 19:29:46 +0100

    When building bzip2 also copy actual dll at the end

--- build_bzip2.bat
+++ build_bzip2.bat
@@ -7,4 +7,5 @@ if %argC% equ 1 (set TOOLSET="v142") else (set TOOLSET=%2)
 
 devenv bzip2\libbz2.dsp /Upgrade
 msbuild bzip2\libbz2.vcxproj /p:Configuration=Release /p:Platform="Win32" /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=10.0.17763.0 /p:PreferredToolArchitecture=Win32
+copy bzip2\libbz2.dll %1\
 copy bzip2\Release\libbz2.* %1\
