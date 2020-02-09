63845fc7163c95c6cd2d732d7b07fe2e0526a13c
Tobias Klein <contact@tklein.info>
2020-02-09 15:12:52 +0100

    Use Visual Studio 2019

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ REM *** ZLIB ***
 
 del /s /f /q zlib\build
 mkdir zlib\build
-cmake -B zlib\build -G "Visual Studio 15 2017" -A Win32 zlib
+cmake -B zlib\build -G "Visual Studio 16 2019" -A Win32 zlib
 devenv zlib\build\zlib.sln /Build Release
