7ce62d262ccd1b9f48f18ccff1565ac95d775e2f
Tobias Klein <contact@tklein.info>
2020-02-09 19:46:23 +0100

    Use msbuild for building icu

--- build_icu.bat
+++ build_icu.bat
@@ -1,9 +1,13 @@
 REM *** ICU ***
 
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+if %argC% equ 1 (set TOOLSET="v142") else (set TOOLSET=%2)
+
 devenv icu\icu4c\source\allinone\allinone.sln /Upgrade
-devenv icu\icu4c\source\allinone\allinone.sln /Build Release /Project i18n
-devenv icu\icu4c\source\allinone\allinone.sln /Build Release /Project io
-devenv icu\icu4c\source\allinone\allinone.sln /Build Release /Project uconv
+msbuild icu\icu4c\source\allinone\allinone.sln /p:Configuration=Release /p:Platform="Win32" /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=10.0.17763.0 /p:PreferredToolArchitecture=Win32
+
 copy icu\icu4c\bin\*.dll %1\
 copy icu\icu4c\lib\*.lib %1\
 copy icu\icu4c\lib\*.pdb %1\
