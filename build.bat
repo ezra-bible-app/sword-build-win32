c7ce7e10fecf9a55073376d7c33ebf592673f0f6
Tobias Klein <contact@tklein.info>
2020-02-09 15:10:32 +0100

    Use Win32 architecture when building

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 REM *** ZLIB ***
 
+del /s /f /q zlib\build
 mkdir zlib\build
-cmake -B zlib\build -G "NMake Makefiles" zlib
-(cd zlib\build && nmake)
+cmake -B zlib\build -G "Visual Studio 15 2017" -A Win32 zlib
+devenv zlib\build\zlib.sln /Build Release
