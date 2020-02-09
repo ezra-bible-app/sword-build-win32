29d8aba81791f57135578945aa3f0b01509a41e0
Tobias Klein <contact@tklein.info>
2020-02-09 19:28:28 +0100

    Build only specific icu projects

--- build_icu.bat
+++ build_icu.bat
@@ -1,7 +1,9 @@
 REM *** ICU ***
 
 devenv icu\icu4c\source\allinone\allinone.sln /Upgrade
-devenv icu\icu4c\source\allinone\allinone.sln /Build Release
+devenv icu\icu4c\source\allinone\allinone.sln /Build Release /Project i18n
+devenv icu\icu4c\source\allinone\allinone.sln /Build Release /Project io
+devenv icu\icu4c\source\allinone\allinone.sln /Build Release /Project uconv
 copy icu\icu4c\bin\*.dll %1\
 copy icu\icu4c\lib\*.lib %1\
 copy icu\icu4c\lib\*.pdb %1\
