f9b1c7e65b08c084ee686eb315f2f6ccf9634640
Tobias Klein <contact@tklein.info>
2020-02-09 18:24:03 +0100

    Add debug statements to build_xz.bat; Fix build_icu.bat

--- build_icu.bat
+++ build_icu.bat
@@ -1,3 +1,6 @@
 REM *** ICU ***
 
 devenv icu\icu4c\source\allinone\allinone.sln /Build Release
+copy icu\icu4c\bin\*.dll %1\
+copy icu\icu4c\lib\*.lib %1\
+copy icu\icu4c\lib\*.pdb %1\
--- build_xz.bat
+++ build_xz.bat
@@ -6,4 +6,8 @@ for %%x in (%*) do Set /A argC+=1
 if %argC% equ 1 (set TOOLSET="v142") else (set TOOLSET=%2)
 
 msbuild xz\windows\vs2017\xz_win.sln /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=10.0.17763.0 /p:PreferredToolArchitecture=Win32
+dir xz\windows
+dir xz\windows\vs2017\Release
+dir xz\windows\vs2017\Release\Win32
+dir xz\windows\vs2017\Release\Win32\liblzma_dll
 copy xz\windows\vs2017\Release\Win32\liblzma_dll\liblzma.* %1\
