7e32d0f40df284528f2726c9e970b6867e9cb36c
Tobias Klein <contact@tklein.info>
2020-02-09 15:47:48 +0100

    Fixes for building zlib and liblzma with different batch files

--- build_zlib.bat
+++ build_zlib.bat
@@ -4,7 +4,3 @@ del /s /f /q zlib\build
 mkdir zlib\build
 cmake -B zlib\build -G "Visual Studio 16 2019" -A Win32 zlib
 devenv zlib\build\zlib.sln /Build Release
-
-
-REM *** XZ ***
-msbuild xz\windows\vs2017\liblzma_dll.vcxproj /p:PlatformToolset=v142
