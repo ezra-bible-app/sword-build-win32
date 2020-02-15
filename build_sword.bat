a2a836c5584f0326dc9d851754c80bc2fb6a275b
Tobias Klein <contact@tklein.info>
2020-02-15 09:20:26 +0100

    Update build scripts for icu and sword

--- build_sword.bat
+++ build_sword.bat
@@ -3,7 +3,12 @@ REM *** SWORD ***
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 
-if %argC% equ 1 (set VS="16 2019") else (set VS=%2)
+if %argC% equ 1 (
+    set VS="16 2019"
+) else (
+    set VS=%2
+    set VS=%VS:"=%
+)
 
 del /s /f /q sword\build
 mkdir sword\build
@@ -13,7 +18,7 @@ cmake -B sword\build -G "Visual Studio %VS%" -A Win32 ^
       -DBZIP2_INCLUDE_DIR=%cd%\bzip2 ^
       -DXZ_LIBRARY=%cd%\output\liblzma.lib ^
       -DXZ_INCLUDE_DIR=%cd%\xz\src\liblzma\api ^
-      -DICU_LIBRARY=%cd%\output\icudt.lib;%cd%\output\icuin.lib;%cd%\output\icuio.lib;%cd%\output\icuuc.lib ^
+      -DICU_LIBRARY=%cd%\output\icuin.lib;%cd%\output\icuuc.lib ^
       -DICU_INCLUDE_DIR=%cd%\icu\icu4c\include ^
       -DCURL_LIBRARY=%cd%\output\libcurl_imp.lib ^
       -DCURL_INCLUDE_DIR=%cd%\curl\include ^
