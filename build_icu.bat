bbf0fb90dae4bce536749ad64ca28c602ade8158
Tobias Klein <contact@tklein.info>
2020-02-15 10:11:07 +0100

    Build all ICU components again

--- build_icu.bat
+++ build_icu.bat
@@ -8,7 +8,7 @@ for %%x in (%*) do Set /A argC+=1
 if %argC% equ 3 (set TOOLSET=%3) else (set TOOLSET="v142")
 
 devenv icu\icu4c\source\allinone\allinone.sln /Upgrade
-msbuild icu\icu4c\source\allinone\allinone.sln /p:Configuration=Release /p:Platform="Win32" /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=10.0.17763.0 /p:PreferredToolArchitecture=Win32 /t:common;i18n
+msbuild icu\icu4c\source\allinone\allinone.sln /p:Configuration=Release /p:Platform="Win32" /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=10.0.17763.0 /p:PreferredToolArchitecture=Win32
 
 copy icu\icu4c\bin\*.dll %2\
 copy icu\icu4c\lib\*.lib %2\
