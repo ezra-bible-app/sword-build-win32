cbf233d76cc5ea2ca9ff8071b3b33952f5d7aa53
Tobias Klein <contact@tklein.info>
2020-02-15 13:56:38 +0100

    Fix zlib build script (only build core zlib)

--- build_zlib.bat
+++ build_zlib.bat
@@ -11,11 +11,11 @@ for %%x in (%*) do Set /A argC+=1
 if %argC% equ 3 (
     set VS=%3
     set VS=%VS:"=%
-    cmake -B curl\build -G "Visual Studio %VS%" -A Win32 curl
+    cmake -B zlib\build -G "Visual Studio %VS%" -A Win32 zlib
 ) else (
-    cmake -B curl\build -G "Visual Studio 16 2019" -A Win32 curl
+    cmake -B zlib\build -G "Visual Studio 16 2019" -A Win32 zlib
 )
 
-devenv zlib\build\zlib.sln /Build Release
+devenv zlib\build\zlib.sln /Build Release /Project zlib
 copy zlib\build\Release\zlib.dll %2\
 copy zlib\build\Release\zlib.lib %2\
