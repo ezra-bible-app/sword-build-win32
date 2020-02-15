89c6048a231e591c18e1aeafeadcd0471d429343
Tobias Klein <contact@tklein.info>
2020-02-15 10:13:31 +0100

    Switch to ICU 65

--- build_all.bat
+++ build_all.bat
@@ -3,4 +3,4 @@ call build_zlib.bat v1.2.9 output
 call build_xz.bat v5.2.4 output
 call build_bzip2.bat bzip2-1.0.8 output
 call build_curl.bat curl-7_68_0 output
-call build_icu.bat release-63-2 output
+call build_icu.bat release-65-1 output
