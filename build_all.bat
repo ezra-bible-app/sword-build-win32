6613bcc14bdde09f684e2aba03cf5e39aef97731
Tobias Klein <contact@tklein.info>
2020-02-15 13:23:21 +0100

    Rename release zip file; Include all projects in release build again;

--- build_all.bat
+++ build_all.bat
@@ -1,6 +1,6 @@
 mkdir output
 call build_zlib.bat v1.2.9 output
-REM call build_xz.bat v5.2.4 output
-REM call build_bzip2.bat bzip2-1.0.8 output
-REM call build_curl.bat curl-7_68_0 output
-REM call build_icu.bat release-65-1 output
+call build_xz.bat v5.2.4 output
+call build_bzip2.bat bzip2-1.0.8 output
+call build_curl.bat curl-7_68_0 output
+call build_icu.bat release-65-1 output
