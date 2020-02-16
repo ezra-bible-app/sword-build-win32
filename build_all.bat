161d9ca1d4c4849487cf62dd1ffa22890d658605
Tobias Klein <contact@tklein.info>
2020-02-16 08:41:54 +0100

    Use dist folder for storing build output; Copy sword headers into dist/include folder at the end of the build process

--- build_all.bat
+++ build_all.bat
@@ -1,9 +1,12 @@
 mkdir build
-mkdir build\lib
+mkdir dist
+mkdir dist\lib
 
-call build_zlib.bat v1.2.9 build\lib
-call build_xz.bat v5.2.4 build\lib
-call build_bzip2.bat bzip2-1.0.8 build\lib
-call build_curl.bat curl-7_68_0 build\lib
-call build_icu.bat release-65-1 build\lib
-call build_sword.bat e34fd3 build\lib
+call build_zlib.bat v1.2.9 dist\lib
+call build_xz.bat v5.2.4 dist\lib
+call build_bzip2.bat bzip2-1.0.8 dist\lib
+call build_curl.bat curl-7_68_0 dist\lib
+call build_icu.bat release-65-1 dist\lib
+call build_sword.bat e34fd3 dist\lib
+
+xcopy sword\include dist\include /E/H
