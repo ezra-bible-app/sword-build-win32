75619fb2b89e4bac54e6f401d1f848667ce67e4a
Tobias Klein <contact@tklein.info>
2020-09-21 17:54:45 +0200

    Add CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS definition for building sword

--- build_sword.bat
+++ build_sword.bat
@@ -16,6 +16,7 @@ if %argC% equ 3 (
 )
 
 cmake -B build\sword -G "Visual Studio %VS%" -A Win32 ^
+      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
       -DBZIP2_LIBRARY=%cd%\%2\libbz2.lib ^
       -DBZIP2_INCLUDE_DIR=%cd%\bzip2 ^
       -DXZ_LIBRARY=%cd%\%2\liblzma.lib ^
