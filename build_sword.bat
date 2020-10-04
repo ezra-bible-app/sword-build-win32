11e4e37e5cef49b874ba31542939adba51cf0a6c
Tobias Klein <contact@tklein.info>
2020-10-04 10:18:28 +0200

    Explicitly define ICU libraries when configuring CMake for SWORD

--- build_sword.bat
+++ build_sword.bat
@@ -16,13 +16,17 @@ if %argC% equ 3 (
 )
 
 cmake -B build\sword -G "Visual Studio %VS%" -A Win32 ^
+      -DICU_DEBUG=ON ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
       -DBZIP2_LIBRARY=%cd%\%2\libbz2.lib ^
       -DBZIP2_INCLUDE_DIR=%cd%\bzip2 ^
       -DXZ_LIBRARY=%cd%\%2\liblzma.lib ^
       -DXZ_INCLUDE_DIR=%cd%\xz\src\liblzma\api ^
-      -DICU_LIBRARY=%cd%\%2\icudt.lib;%cd%\%2\icuin.lib;%cd%\%2\icuuc.lib ^
       -DICU_INCLUDE_DIR=%cd%\icu\icu4c\include ^
+      -DICU_DATA_LIBRARY=%cd%\%2\icudt.lib ^
+      -DICU_I18N_LIBRARY=%cd%\%2\icuin.lib ^
+      -DICU_IO_LIBRARY=%cd%\%2\icuio.lib ^
+      -DICU_UC_LIBRARY=%cd%\%2\icuuc.lib ^
       -DCURL_LIBRARY=%cd%\%2\libcurl_imp.lib ^
       -DCURL_INCLUDE_DIR=%cd%\curl\include ^
       sword
