33cf0d5d7443f6f5d5e7afa01fe09c07380c3bf9
Tobias Klein <contact@tklein.info>
2020-10-04 10:45:43 +0200

    Configure ICU by providing ICU_ROOT dir to CMake

--- build_sword.bat
+++ build_sword.bat
@@ -22,11 +22,8 @@ cmake -B build\sword -G "Visual Studio %VS%" -A Win32 ^
       -DBZIP2_INCLUDE_DIR=%cd%\bzip2 ^
       -DXZ_LIBRARY=%cd%\%2\liblzma.lib ^
       -DXZ_INCLUDE_DIR=%cd%\xz\src\liblzma\api ^
+      -DICU_ROOT=%cd%\icu ^
       -DICU_INCLUDE_DIR=%cd%\icu\icu4c\include ^
-      -DICU_DATA_LIBRARY=%cd%\%2\icudt.lib ^
-      -DICU_I18N_LIBRARY=%cd%\%2\icuin.lib ^
-      -DICU_IO_LIBRARY=%cd%\%2\icuio.lib ^
-      -DICU_UC_LIBRARY=%cd%\%2\icuuc.lib ^
       -DCURL_LIBRARY=%cd%\%2\libcurl_imp.lib ^
       -DCURL_INCLUDE_DIR=%cd%\curl\include ^
       sword
