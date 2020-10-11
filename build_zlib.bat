30adbc25cd5c6f8d54e9945c4f291390ac0eee70
Tobias Klein <contact@tklein.info>
2020-10-11 18:20:12 +0200

    zlib: generate debug symbols

--- build_zlib.bat
+++ build_zlib.bat
@@ -16,7 +16,7 @@ if %argC% equ 3 (
     cmake -B build\zlib -G "Visual Studio 16 2019" -A Win32 zlib
 )
 
-devenv build\zlib\zlib.sln /Build Release /Project zlib
+devenv build\zlib\zlib.sln /Build Release /Project zlib /DEBUG:FULL
 dir build\zlib\Release\
 copy build\zlib\Release\zlib.dll %2\
 copy build\zlib\Release\zlib.lib %2\
