c157c9cd8f53e7c67bbe45b36aa551c2455aa694
Tobias Klein <contact@tklein.info>
2021-01-24 19:09:50 +0100

    Add UNICODE definitions to CMake configuration of SWORD

--- build_sword.bat
+++ build_sword.bat
@@ -25,6 +25,8 @@ cmake -B build\sword -G "Visual Studio %VS%" -A Win32 ^
       -DICU_INCLUDE_DIR=%cd%\icu\icu4c\include ^
       -DCURL_LIBRARY=%cd%\%2\libcurl_imp.lib ^
       -DCURL_INCLUDE_DIR=%cd%\curl\include ^
+      -DUNICODE ^
+      -D_UNICODE ^
       sword
 
 devenv build\sword\libsword.sln /Build Debug /Project sword
