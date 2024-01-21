a9747f1cf7d38f1bc45db6fb0112367a31e42f62
Tobias Klein <contact@tklein.info>
2024-01-21 14:53:00 +0100

    Remove build dependency on ICU

--- build_all.bat
+++ build_all.bat
@@ -6,7 +6,6 @@ call build_zlib.bat v1.2.9 dist\lib
 call build_xz.bat v5.2.4 dist\lib
 call build_bzip2.bat bzip2-1.0.8 dist\lib
 call build_curl.bat curl-7_68_0 dist\lib
-call build_icu.bat release-65-1 dist\lib
 call build_sword.bat 574325 dist\lib
 
 xcopy sword\include dist\include /E /I
